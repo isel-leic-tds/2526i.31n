6d2dfc8c35a84b465047fa7849589b7cc4ede591
Afonso Remédios <afonso.remedios@isel.pt>
2025-09-24 21:23:39 +0100

    Add isLeapYear, add isLastDayOfMont and start implementing addDays

--- _2_Date/src/main/kotlin/isel/tds/Date.kt
+++ _2_Date/src/main/kotlin/isel/tds/Date.kt
@@ -39,10 +39,46 @@ package isel.tds.isel.tds
 //    }
 //}
 
+//class Date(val year: Int, val month: Int = 1, val day: Int = 1){
+//    val isLeapYear: Boolean = year%4 == 0 && year%100!=0 || year%400 == 0
+//
+//    init{
+//        require( year>0){"year is negative or zero"}
+//        require(month>0 && month <=12 ){"month is negative or zero"}
+//        require(day>0){"day is negative or zero"}
+//    }
+//}
+
 class Date(val year: Int, val month: Int = 1, val day: Int = 1){
+
+
     init{
         require( year>0){"year is negative or zero"}
-        require(month>0){"month is negative or zero"}
-        require(day>0){"day is negative or zero"}
+        require(month>0 && month <=12 ){"month is negative or zero"}
+        require(day>0 && day <= lastDayOfMonth){"day is negative or zero"}
     }
 }
+
+val Date.isLeapYear: Boolean
+    get() = year%4 == 0 && year%100!=0 || year%400 == 0
+
+private val daysOfMonth = listOf(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
+val Date.isLastDayOfMonth: Boolean
+    get() = day == this.lastDayOfMonth
+
+private val Date.lastDayOfMonth: Int
+    get() = if( month==2 && this.isLeapYear) 29 else daysOfMonth[this.month-1]
+
+fun Date.addDays(daystoAdd: Int) = Date(1,1,1)
+
+operator fun Date.plus(daystoAdd: Int) = Date(1,1,1)
+
+
+
+
+
+
+
+
+
+
--- _2_Date/src/test/kotlin/isel/tds/DateTest.kt
+++ _2_Date/src/test/kotlin/isel/tds/DateTest.kt
@@ -1,9 +1,17 @@
 package isel.tds
 
 import isel.tds.isel.tds.Date
-import org.junit.jupiter.api.Assertions.*
+import isel.tds.isel.tds.addDays
+import isel.tds.isel.tds.isLastDayOfMonth
+import isel.tds.isel.tds.isLeapYear
+import isel.tds.isel.tds.plus
 import kotlin.test.Test
+import kotlin.test.assertEquals
 import kotlin.test.assertFailsWith
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+
+
 
 class DateTest {
 
@@ -41,6 +49,71 @@ class DateTest {
         assertFailsWith<IllegalArgumentException> {Date( 0)}
         assertFailsWith<IllegalArgumentException> {Date(2025, 0)}
         assertFailsWith<IllegalArgumentException> {Date(2025, 1, 0)}
+    }
 
+    @Test
+    fun `test date creation with invalid days`(){
+        assertFailsWith<IllegalArgumentException> {Date( 2025, 2, 29)}
+        assertFailsWith<IllegalArgumentException> {Date(2025, 1, 32)}
+        assertFailsWith<IllegalArgumentException> {Date(2025, 4, 31)}
+    }
+
+    @Test
+    fun `test date creation with month greater than 12`(){
+        assertFailsWith<IllegalArgumentException> {Date(2025, 13)}
+        assertFailsWith<IllegalArgumentException> {Date(2025, 13, 3)}
+    }
+
+    @Test
+    fun `test date is leap year`(){
+        val date = Date(2024, 1,1)
+        assertTrue( date.isLeapYear)
+
+        val dateLeapYear400Multiple = Date(2400, 1,1)
+        assertTrue( dateLeapYear400Multiple.isLeapYear)
+    }
+
+    @Test
+    fun `test date isn't leap year`(){
+        val date = Date(2025, 1,1)
+        assertFalse( date.isLeapYear)
+
+        val dateNotLeapYear100Multiple = Date(2100, 1,1)
+        assertFalse( dateNotLeapYear100Multiple.isLeapYear)
+    }
+
+    @Test
+    fun `test date is last day of month true`(){
+        val dateLastDayOfJan = Date(2024, 1,31)
+        assertTrue( dateLastDayOfJan.isLastDayOfMonth)
+
+        val dateLastDayOfMonthInLeapYearForFeb = Date(2024, 2,29)
+        assertTrue( dateLastDayOfMonthInLeapYearForFeb.isLastDayOfMonth)
+
+        val dateLastDayOfMonthInNonLeapYearForFeb = Date(2025, 2,28)
+        assertTrue( dateLastDayOfMonthInNonLeapYearForFeb.isLastDayOfMonth)
+    }
+
+    @Test
+    fun `test date is last day of month false`(){
+        val dateLastDayOfJan = Date(2024, 1,30)
+        assertFalse( dateLastDayOfJan.isLastDayOfMonth)
+
+        val dateLastDayOfMonthInLeapYearForFeb = Date(2024, 2,28)
+        assertFalse( dateLastDayOfMonthInLeapYearForFeb.isLastDayOfMonth)
+
+        val dateLastDayOfMonthInNonLeapYearForFeb = Date(2025, 2,27)
+        assertFalse( dateLastDayOfMonthInNonLeapYearForFeb.isLastDayOfMonth)
+    }
+
+    @Test
+    fun `test add days`(){
+        val dateAdd = Date(2024, 1,1).addDays(5)
+        assertEquals(6, dateAdd.day)
+
+        val dateAddUsingOperator = Date(2024, 1,1) + 5
+        assertEquals(6, dateAddUsingOperator.day)
     }
 }
+
+
